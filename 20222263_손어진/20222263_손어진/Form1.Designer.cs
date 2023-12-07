29209aacb4544e436d1d4df65c665d1ad91b1c4c
Blind2000 <guswls001133@gmail.com>
2023-12-07 20:53:10 +0900

    Form2.cs plus

--- 20222263_손어진/20222263_손어진/Form1.Designer.cs
+++ 20222263_손어진/20222263_손어진/Form1.Designer.cs
@@ -132,7 +132,6 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
